12952932f2e41cdb7efd041ae140c9ff59772fb6
Vasileva Masha <Vasilevamasha1982@gmail.com>
2023-12-16 17:08:07 +0300

    seminar1

--- seminar/1/script.js
+++ seminar/1/script.js
@@ -1,1 +1,10 @@
 console.log("Текст из script.js!");
+let num = 123;
+console.log(num);
+let a = 5;
+a = 7;
+console.log(a);
+let b = 1 + 2 + 3;
+console.log(b);
+//семинар;
+/* многострочный комментарий*/
