b00410be61c6f53f11436515d6868a397a6a9ec5
Vasileva Masha <Vasilevamasha1982@gmail.com>
2023-12-22 21:31:02 +0300

    Задание 2

--- seminar/3/script.js
+++ seminar/3/script.js
@@ -11,7 +11,8 @@
 отрицательное. В первом случае пусть функция выводит
 в консоль текст '+++', а во втором '---'.*/
 //1
-function userData(name, lastName, age) {
+
+/*function userData(name, lastName, age) {
   alert(`Привет ${lastName} ${name} твой возраст ${age}`);
 }
 userData("Петр", "Петров", 35);
@@ -30,4 +31,42 @@ function sravneniye(num) {
     console.log("++++++");
   }
 }
-sravneniye(num);
+sravneniye(num);*/
+
+/*Задание 2 (тайминг 15 минут)
+1. Сделайте функцию, которая параметрами принимает 3 числа и
+выводит в консоль сумму этих чисел.
+2. С помощью созданной вами функции выведите в консоль сумму
+значений этих переменных.
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+
+
+1. Дана фукнция
+function func(num = 5) {
+console.log(num * num);
+}
+Расскажите, каким будет результат каждого из вызовов функции.
+func(2);
+func(3);
+func();*/
+
+let num1 = Number(prompt("Введите 1 число   "));
+let num2 = Number(prompt("Введите 2 число   "));
+let num3 = Number(prompt("Введите 3 число   "));
+function sum(num1, num2, num3) {
+  alert(`Сумма чисел равена ${num1 + num2 + num3}`);
+}
+sum(num1, num2, num3);
+let param1 = 1;
+let param2 = 2;
+let param3 = 3;
+sum(param1, param2, param3);
+
+function func(num = 5) {
+  console.log(num * num);
+}
+func(2);
+func(3);
+func();
